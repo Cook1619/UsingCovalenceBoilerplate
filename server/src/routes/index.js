760a7bbad8e697eff781e53fa11c6fb646f44566
Cook1619 <cook6348@gmail.com>
2018-07-10 12:59:33 -0500

    first commit, my get request to /api/blogs is not working

---
+++ server/src/routes/index.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import peopleRouter from './people';
+import blogRouter from './blogs';
+
+let router = Router();
+
+router.use('/people', peopleRouter);
+router.use('./blogs', blogRouter);
+
+export default router;
