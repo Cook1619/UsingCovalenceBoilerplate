23c38f9c8098060dfb5a1a757459f766a1543db6
Cook1619 <cook6348@gmail.com>
2018-07-10 18:37:53 -0500

    still getting  a token < in the json position 0 error

--- client/src/components/BlogDetails.jsx
+++ client/src/components/BlogDetails.jsx
@@ -2,12 +2,12 @@ import React  from 'react';
 import { Link } from 'react-router-dom';
 
 let BlogDetails = ({ blogdata }) => {
-    console.log(blogdata);
+    console.log(blogdata.id);
     return (
         <div className="card">
             <div className="card-header">
                 <div className="card-body">{blogdata.title}
-                    <Link key={blogdata.id} className="btn btn-info float-right" to={`/api/blogs/${blogdata.id}`}>More Details</Link>
+                    <Link key={blogdata.id} className="btn btn-info float-right" to={`/api/blogs/`+ blogdata.id}>More Details</Link>
                 </div>
             </div>
         </div>
