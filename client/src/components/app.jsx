e5497753da85b685fa639d8f72df5a8b53beb2fe
Cook1619 <cook6348@gmail.com>
2018-07-11 07:06:41 -0500

    Set up a homepage and it's functionally working

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import BlogList from './BlogList';
 import SingleBlog from './SingleBlog';
+import HomePage from './Homepage'
 import GoodbyeWorld from './goodbye';
 
 class Navigation extends Component {
@@ -10,10 +11,12 @@ class Navigation extends Component {
         return (
             <Router>
                 <Fragment>
-                    <Link to="/goodbye">Goodbye</Link>
+                    <nav>
+                        <Link to="/api/blogs">Blogs</Link>
+                    </nav>
                     <Switch>
-                        <Route exact path="/" component={BlogList} />
-                        <Route path="/goodbye" component={GoodbyeWorld} />
+                        <Route exact path="/" component={HomePage} />
+                        <Route exact path="/api/blogs" component={BlogList} />
                         <Route path="/api/blogs/:id" component={SingleBlog} />
                     </Switch>
                 </Fragment>
